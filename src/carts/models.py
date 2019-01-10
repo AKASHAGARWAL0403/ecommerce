44ae9aa9458d180568ea16873f8a87d5ad43d58f
akash <akashagarwal0403@gmail.com>
2019-01-10 20:33:40 +0530

    view ajax done

--- src/carts/models.py
+++ src/carts/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from product.models import Variation
 from django.conf import settings
 from decimal import Decimal
-from django.db.models.signals import pre_save, post_save
+from django.db.models.signals import pre_save, post_save, post_delete
 # Create your models here.
 
 class CartItem(models.Model):
@@ -29,13 +29,15 @@ def post_sub_total(sender,instance,*args,**kwrags):
 	instance.cart.get_total()
 
 post_save.connect(post_sub_total,sender=CartItem)
+post_delete.connect(post_sub_total,sender=CartItem)
+
 
 class Cart(models.Model):
 	user = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,null=True,blank=True)
 	items = models.ManyToManyField(Variation,through=CartItem)
 	timestamp = models.DateTimeField(auto_now=False,auto_now_add=True)
-	upadted = models.DateTimeField(auto_now_add=False,auto_now=True)
-	sub_total = models.DecimalField(max_digits=100,decimal_places=2)
+	updated = models.DateTimeField(auto_now_add=False,auto_now=True)
+	sub_total = models.DecimalField(max_digits=100,decimal_places=2,null=True)
 
 	def __str__(self):
 		return str(self.id)
--- src/carts/views.py
+++ src/carts/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render , get_object_or_404
-from django.http import HttpResponseRedirect,Http404
+from django.http import HttpResponseRedirect,Http404,JsonResponse
 from django.views.generic.base import View
 from .models import Cart,CartItem
 from product.models import Variation
 from django.views.generic.detail import SingleObjectMixin
+
+
+
 
 
 class CartView(SingleObjectMixin,View):
@@ -19,7 +22,6 @@ class CartView(SingleObjectMixin,View):
 			cart = Cart()
 			cart.save()
 			cart_id = cart.id
-			print("gbdfvdc")
 			self.request.session["cart_id"] = cart.id
 		cart = Cart.objects.get(id=cart_id)
 		if self.request.user.is_authenticated:
@@ -30,25 +32,48 @@ class CartView(SingleObjectMixin,View):
 	def get(self,request,*args,**kwargs):
 		cart = self.get_object()
 		item_id = request.GET.get("item")
-		delete = request.GET.get("delete")
+		delete = request.GET.get("delete",False)
+		item_added = False
 		if item_id:
 			item_instance = get_object_or_404(Variation,id=item_id)
 			print(item_instance)
 			qty = request.GET.get("qty",1)
 
-		try:
-			if int(qty) < 1:
-				delete = True
-		except:
-			raise Http404
-		print(cart , item_instance)
-		cart_item = CartItem.objects.get_or_create(cart=cart,item=item_instance)[0]
-		if delete:
-			cart_item.delete()
-		else:
-			cart_item.quantity = qty
-			cart_item.save()
+			try:
+				if int(qty) < 1:
+					delete = True
+			except:
+				raise Http404
+			print(cart , item_instance)
+			cart_item , created = CartItem.objects.get_or_create(cart=cart,item=item_instance)
 
+			if created:
+				item_added = True
+
+			if delete:
+				cart_item.delete()
+				delete = True
+			else:
+				cart_item.quantity = qty
+				cart_item.save()
+
+		if request.is_ajax():
+			try:
+				line_total = cart_item.line_item_total
+			except:
+				line_total = None
+			try:
+				total = cart_item.cart.sub_total
+			except:
+				total = None
+
+			data = {
+				"item_added" : item_added,
+				"deleted" : delete,
+				"line_total" : line_total,
+				"total" : total
+			}
+			return JsonResponse(data)
 		context = {
 			"objects" : self.get_object()
 		}
